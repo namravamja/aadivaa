42b9d1df8429ed784bd0db272de0f6a7f744ee48
Namra Vamja <namravamja00@gmail.com>
2025-06-10 18:32:17 +0530

    feat: Update FrontendLayout to render HomePage instead of children

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,13 @@
 import type { ReactNode } from "react";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
+import HomePage from "@/components/Homepage/page";
 
-export default function FrontendLayout({ children }: { children: ReactNode }) {
+export default function FrontendLayout() {
   return (
     <>
       <Navbar />
-      {children}
+      <HomePage />
       <Footer />
     </>
   );
