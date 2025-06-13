48681ebef1fb65682e55ea163cad22b0b72b5eeb
Namra Vamja <namravamja00@gmail.com>
2025-06-13 13:30:12 +0530

    fix: Update CORS origin to allow all origins

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,7 +23,7 @@ app.set("trust proxy", 1);
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: true,
     credentials: true,
   })
 );
