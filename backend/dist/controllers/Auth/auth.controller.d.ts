67a834a4c2704e9540d703a8d4ffc0b4c6f84175
Namra Vamja <namravamja00@gmail.com>
2025-06-17 22:31:40 +0530

    feat: Remove unused authentication controller files

--- backend/dist/controllers/Auth/auth.controller.d.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express";
-export declare const signupBuyer: (req: Request, res: Response) => Promise<void>;
-export declare const loginBuyer: (req: Request, res: Response) => Promise<void>;
-export declare const signupArtist: (req: Request, res: Response) => Promise<void>;
-export declare const loginArtist: (req: Request, res: Response) => Promise<void>;
